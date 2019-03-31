b9400100154cb924e568561f568517c1d96a7a83
Esra Oktav Dinler <oktavesra@gmail.com>
2019-03-31 17:18:52 +0200

    :zap::heavy_plus_sign: Added ability to get search results based on selected genre, (temporary but atm as option select) + Added it to the routes/web.php and Pages controller

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -97,8 +97,51 @@ class PagesController extends Controller
         }
     }
 
+
+
     public function dashboard()
     {
         return view('pages.dashboard');
     }
+
+
+    public function searchResultsGenre(Request $request)
+    {
+        // dd($request->all());
+        // dd($request->action);
+
+        if(isset($request->movieGenre) && !empty($request->movieGenre)) {
+            
+            $api = env('TMDB_API_KEY');
+            $searchedMovieName2 = $request->movieGenre;
+            $url = "https://api.themoviedb.org/3/discover/movie?api_key={$api}&with_genres={$searchedMovieName2}";
+
+            $ch = curl_init();
+
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_FAILONERROR, true);
+
+            $response = curl_exec($ch);
+
+            if (curl_error($ch)) {
+                $error_msg = curl_error($ch);
+            }
+
+            curl_close($ch);
+
+            if (!isset($error_msg)){
+                $decodedResponse = json_decode($response, true);
+            }
+        } else {
+            $error_msg = "No data in input box";
+        }
+        if (isset($error_msg)) {
+            return view('pages.searchResultsGenre')->with('error_msg', $error_msg);
+        } else {
+            return view('pages.searchResultsGenre')->with('data', $decodedResponse['results']);
+        }
+    }
+
 }
---
+++ resources/views/pages/searchResultsGenre.blade.php
@@ -0,0 +1,68 @@
+@extends('layouts.app')
+
+@section('content')
+
+
+
+<div class="page-content">
+
+
+    <div class="s-results-container">     
+        @if(isset($data) && count($data) > 0)
+            <div class="search-results-container">
+                <div class="search-results-content">
+                    <div class="search-results">
+                    @foreach($data as $value)
+                    
+                    <div class="search-result-item">
+                        {{-- <a href='https://www.themoviedb.org/movie/{{$value['id']}}'> --}}
+                        <a href='{{ url('details/' . $value['id']) }}'>
+                            
+                            @if(isset($value['poster_path']))
+                                {{--<img src='https://image.tmdb.org/t/p/w1280/{{$value['poster_path']}}'/>--}}
+                                <div class="search-r-image" style="background: url('https://image.tmdb.org/t/p/w1280/{{$value['poster_path']}}'); height: 300px; width: 200px; background-size: cover;">
+                               
+                                    @if(isset($value['overview']))
+                                    <p> <span style="font-weight: bold; font-size: 18px;"> {{$value['title']}} </span><br/><br/>{{ $value['overview'] }}</p>
+                                    @endif
+                                </div>
+                            @else
+                                <img src='{{ asset('images/movPlaceholder.png') }}' style='height: 300px;'/>
+                            @endif
+                
+                            <p class="result-title">
+                                {{$value['title']}} 
+                            </p>
+                             
+                            @if(strlen($value['release_date']) > 1)
+                                <p class="result-release">
+                                    {{substr($value['release_date'], 0 ,-6)}}
+                                </p>
+                            @endif
+                      
+
+                                
+                        </a>
+                    </div>  
+        @endforeach
+                </div>
+            </div>
+        </div>
+
+
+
+
+        </div>
+
+
+
+        @elseif(isset($error_msg))
+            <h3>
+                {{$error_msg}}
+            </h3>
+        @endif
+        
+    </div>
+</div>
+    
+@endsection
