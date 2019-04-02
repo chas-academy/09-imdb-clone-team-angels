eb8b87ee94165c38bf73c8ed1d1a15776fa01357
Esra Oktav Dinler <oktavesra@gmail.com>
2019-04-02 23:12:36 +0200

    :zap: deleted unnecessary code and moved searchfield and genre options to left side of the nav

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,183 +37,172 @@
             var instances = M.FormSelect.init(elems, {});
         });
     </script>
-
 </head>
+
 <body>
     <div id="app">
-        {{-- <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Cineo') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav> --}}
-
-    <nav class="flex-row sp-be">
-        <div class="nav-flex-1 flex-col sp-ar">
-            <div>
-                <i class="fas fa-bars"></i>
-            </div>
-        </div>
-
-        <div class="nav-flex-2">
-            <div class="title-logo">
-                <!-- <img src="{{ asset('images/cineoWhite.png') }}"> -->
-                <a href="/"><h1 style="color:white">cineo</h1></a>
-            </div>
-        </div>
-
-        <div class="nav-flex-3 flex-col sp-ar">
-            <div class="inline flex-row">
-
-               <div class="search">
-                    <form class="searchform" action="{{url('/searchresults')}}" method="POST">
-                        @csrf
-                        <div class="search-input-field">
-                            <input id="search-input" name="movieName" type="text" placeholder="Search" required>
-                            <button class="item1" type="submit" name="action">
-                                <i class="fas fa-search"></i>
-                            </button>
-                            &nbsp;
-                        </div>
-                        <!-- <div class="row"></div> -->
-                    </form>
-                </div>
-
-                {{--<div class="search">
-                    <form class="searchform" action="{{url('/searchresults')}}" method="POST">
-                        @csrf
-                        <div class="search-input-field">
-                            <input id="search-input" name="movieName2" type="text" placeholder="Search" required>
-                            <button class="item1" type="submit" name="action">
-                                <i class="fas fa-search"></i>
-                            </button>
-                            &nbsp;
-                        </div>
-                        <!-- <div class="row"></div> -->
-                    </form>
-                </div>--}}
-
-                <div class="search">
-                    <form class="searchform" action="{{url('/searchresultsgenre')}}" method="POST">
-                        @csrf
-                        <div class="search-input-field style-selected">
-                            <select id="search-input" name="movieGenre" placeholder="See Movies By Genre" required>
-                            <option value="" disabled selected>Movies By Genre</option> 
-                                <option value="28">Action</option>
-                                <option value="12">Adventure</option>
-                                <option value="16">Animation</option>
-                                <option value="35">Comedy</option>
-                                <option value="80">Crime</option>
-                                <option value="99">Documentary</option>
-                                <option value="18">Drama</option>
-                                <option value="10751">Family</option>
-                                <option value="14">Fantasy</option>
-                                <option value="36">History</option>
-                                <option value="27">Horror</option>
-                                <option value="9640">Mystery</option>
-                                <option value="878">Science Fiction</option>
-                                <option value="10752">War</option>
-                                <option value="37">Western</option>
-                            </select>
-                            <button class="item1" type="submit" name="action">
-                                <i class="fas fa-search"></i>
-                            </button>
-                            &nbsp;
-                        </div>
-                        <!-- <div class="row"></div> -->
-                    </form>
-                </div>
-
-
-
-
-                <div class="s-up-in">
-                    @guest
-                      <button class="item1"><a class="lo-inout" href="{{ route('login') }}">Login</a></button>
-                      {{-- @if (Route::has('register')) --}}
-                      <button class="item1"><a class="lo-inout" href="{{ route('register') }}">Register</a></button>
-                      {{-- @endif --}}
-                    @else
-                     
-                            <a class="lo-inout" href="{{ route('logout') }}"
-                                onclick="event.preventDefault();
-                                document.getElementById('logout-form').submit();
-                                ">
-                                &nbsp;Logout
-                            </a>
-                      
-                            <a class="lo-inout" href="/profile">
-                            &nbsp; Profile
-                            </a>
-                    
-
-                        <button class="item1">
-                            <span><i class="fas fa-user"></i>&nbsp;<b>{{ Auth::user()->name }}</b></span> {{-- Dashboard on click --}}
-                        </button>
-
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+        <nav class="flex-row sp-be">
+            <div class="nav-flex-1 flex-row sp-ev">
+                    <div class="search">
+                        <form class="searchform" action="{{url('/searchresults')}}" method="POST">
                             @csrf
+                            <div class="search-input-field">
+                                <input id="search-input" name="movieName" type="text" placeholder="Search" required>
+                                <button class="item1" type="submit" name="action">
+                                    <i class="fas fa-search"></i>
+                                </button>
+                                &nbsp;
+                            </div>
                         </form>
-                    @endguest
-                
+                    </div>
+                    <div class="search">
+                        <form class="searchform" action="{{url('/searchresultsgenre')}}" method="POST">
+                            @csrf
+                            <div class="search-input-field style-selected">
+                                <select id="search-input" name="movieGenre" placeholder="See Movies By Genre" required>
+                                <option value="" disabled selected>Movies By Genre</option> 
+                                    <option value="28">Action</option>
+                                    <option value="12">Adventure</option>
+                                    <option value="16">Animation</option>
+                                    <option value="35">Comedy</option>
+                                    <option value="80">Crime</option>
+                                    <option value="99">Documentary</option>
+                                    <option value="18">Drama</option>
+                                    <option value="10751">Family</option>
+                                    <option value="14">Fantasy</option>
+                                    <option value="36">History</option>
+                                    <option value="27">Horror</option>
+                                    <option value="9640">Mystery</option>
+                                    <option value="878">Science Fiction</option>
+                                    <option value="10752">War</option>
+                                    <option value="37">Western</option>
+                                </select>
+                                <button class="item1" type="submit" name="action">
+                                    <i class="fas fa-search"></i>
+                                </button>
+                                &nbsp;
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+                <div class="nav-flex-2">
+                    <div class="title-logo">
+                        {{-- <img src="{{ asset('images/cineoWhite.png') }}">  --}}
+                        <a href="/">
+                            <h1 style="color:#ffffff;">cineo</h1>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="nav-flex-3 flex-col sp-ar">
+                    <div class="inline flex-row">
+
+                        <div class="s-up-in">
+                            @guest
+                            <button class="item1"><a class="lo-inout" href="{{ route('login') }}">Login</a></button>
+                            {{-- @if (Route::has('register')) --}}
+                            <button class="item1"><a class="lo-inout" href="{{ route('register') }}">Register</a></button>
+                            {{-- @endif --}}
+                            @else
+                                <a class="lo-inout" href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
+                                    document.getElementById('logout-form').submit();
+                                    ">
+                                    &nbsp;Logout
+                                </a>
+                                <a class="lo-inout" href="/profile">
+                                    &nbsp; Profile
+                                </a>
+                            
+                                <button class="item1">
+                                    <span><i class="fas fa-user"></i>&nbsp;<b>{{ Auth::user()->name }}</b></span> {{-- Dashboard on click --}}
+                                </button>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
+                            @endguest
+                        </div>
+
+                    </div>
                 </div>
             </div>
-        </div>
-
-
-        </div>
-     </nav>
+        </nav>
 
         <main>
             <div class="row">
-                    @yield('content')
-                
+                @yield('content')
             </div>
         </main>
+
+        <footer class="flex-row">
+            <div class="box flex-col">
+                <div>
+                @guest
+                        <button class="item1"><a class="soi-footer" href="{{ route('login') }}">Login</a></button>
+                        {{-- @if (Route::has('register')) --}}
+                        <button class="item1"><a class="soi-footer" href="{{ route('register') }}">&nbsp;Register</a></button>
+                        {{-- @endif --}}
+                    @else
+                        
+                    <a class="" href="{{ route('logout') }}"
+                        onclick="event.preventDefault();
+                        document.getElementById('logout-form').submit();
+                        ">
+                        &nbsp;Logout
+                    </a>
+                        
+                    <a class="soi-footer" href="/dashboard">
+                    &nbsp; Dash
+                    </a>
+                    
+                    <button class="item1">
+                        <span><i class="fas fa-user"></i>&nbsp;<b>{{ Auth::user()->name }}</b></span> {{-- Dashboard on click --}}
+                    </button>
+    
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+                @endguest
+                </div>
+                    
+                <a href="/">
+                    <p>The cineo Group</p>
+                </a>
+                <a href="/">
+                    <p>Contact</p>
+                </a>
+                <a href="/">
+                    <p>Support</p>
+                </a>
+                <a href="/">
+                    <p>About us</p>
+                </a>
+            </div>
+        
+            <div class="box flex-col">
+                <div class="box-line flex-row">
+                    <a href="/">
+                        <i class="fab fa-twitter"></i>
+                    </a>
+                    <a href="/">
+                        <i class="fab fa-youtube"></i>
+                    </a>
+                    <a href="/">
+                        <i class="fab fa-facebook-square"></i>
+                    </a>
+                </div>
+                <div style="height:10px;">
+                </div>
+                <p>Nowhere</p>
+                <p>23728 NW</p>
+                <p>NWAS</p>
+            </div>              
+        </footer>
+        
     </div>
+
 </body>
 </html>
