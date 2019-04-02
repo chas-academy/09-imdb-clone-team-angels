5e82ec35e3a08c3906c9b4155de2e0f29b6795c4
Esra Oktav Dinler <oktavesra@gmail.com>
2019-04-02 17:44:20 +0200

    :art: New styling TEMPORARY and now username is shown at profile etc

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -1,49 +1,55 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Profile</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+    <div class="profile-card flex-col">
+    <div class="profile-header"><span>&nbsp;<b>{{ Auth::user()->name }}'s</b></span> profile  </div>
 
-                    You are logged in!
+         <div class="container-profile">
+
+          
+            <!-- <div class="profile-status">
+                @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
+                @endif
+
+                You are logged in!
+            </div> -->
+        
+            
+            <div class="watch-lists flex-col sp-be">
+                <div>
+            <h3>Watchlists</h3>
+            <ul>
+                @foreach($watchlists as $watchlist)
+                <div>
+                    <form method="POST" action="/watchlists/{{ $watchlist['id'] }}/delete">
+                        @csrf
+
+                        <a href="/watchlists/{{ $watchlist['id'] }}">
+                        <span style="color: black;">●</span> {{ $watchlist['title'] }}
+                        </a>
+                        <button class="list-del-btn" type="submit">✖</button>
+                    </form>
                 </div>
+                @endforeach
+            </ul>
+            </div>
+                <div class="add-new"> 
+                    <h4>Add New Watchlist</h4>
 
-                <h3>Watchlists</h3>
-                <ul>
-                    @foreach($watchlists as $watchlist)
-                    <li>
-                        <form method="POST" action="/watchlists/{{ $watchlist['id'] }}/delete">
-                            @csrf
+                    <form method="POST" action="/watchlists">
+                        @csrf
 
-                            <a href="/watchlists/{{ $watchlist['id'] }}">
-                                {{ $watchlist['title'] }}
-                            </a>
-                            <button class="waves-effect waves-teal btn-flat" type="submit">X</button>
-                        </form>
-                    </li>
-                    @endforeach
-                </ul>
-
-                <h4>Add Watchlist</h4>
-
-                <form method="POST" action="/watchlists">
-                    @csrf
-
-                    <label for="title">Title</label>
-                    <input name="title" />
-                    <button type="submit">Submit</button>
-                </form>
+                        <!-- <label for="title">Title</label> -->
+                        <input placeholder="Watchlist Title..."name="title" />
+                        <button type="submit">Submit</button>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
-</div>
+
 @endsection
