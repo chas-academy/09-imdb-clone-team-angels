28e090abefe3cf4c38fce5e10fdf8cf534399eed
Esra Oktav Dinler <oktavesra@gmail.com>
2019-04-03 17:43:09 +0200

    :zap::art: New styling for profile: Welcome, watchlists & reviews cards added

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -3,9 +3,10 @@
 @section('content')
 
     <div class="profile-card flex-col">
-    <div class="profile-header"><span>&nbsp;<b>{{ Auth::user()->name }}'s</b></span> profile  </div>
+    {{-- <div class="profile-header"><span>&nbsp;<b>{{ Auth::user()->name }}'s</b></span> profile  </div> --}}
+    <div class="profile-header"><h2>{{ Auth::user()->name }}</h2></div>
 
-         <div class="container-profile">
+         <div class="container-profile" style="width: 100vw; height: 100vw; justify-content: space-evenly; display: flex;">
 
           
             <!-- <div class="profile-status">
@@ -21,10 +22,61 @@
             
             <div class="watch-lists flex-col sp-be">
                 <div>
-            <h3>Watchlists</h3>
+                    <h3>Welcome {{ Auth::user()->name }}!</h3>
+                    <ul>
+                        <div>
+                            <h4>Name: {{ Auth::user()->name }}</h4>
+                        </div>
+                        <div>  
+                            <h4>Email: {{ Auth::user()->email }}</h4>
+                        </div>
+                    </ul>
+                </div>
+           
+                {{-- <div class="add-new"> 
+                 
+                </div> --}}
+            </div>
+       
+
+
+            <div class="watch-lists flex-col sp-be">
+                    <div>
+                <h3>Your Watch Lists</h3>
+                <ul>
+                    @foreach($watchlists as $watchlist)
+                    <div class="formform">
+                        <form method="POST" action="/watchlists/{{ $watchlist['id'] }}/delete">
+                            @csrf
+    
+                            <a href="/watchlists/{{ $watchlist['id'] }}">
+                            <span style="color: black;">●</span> {{ $watchlist['title'] }}
+                            </a>
+                            <button class="list-del-btn" type="submit">✖</button>
+                        </form>
+                    </div>
+                    @endforeach
+                </ul>
+                </div>
+                    <div class="add-new"> 
+                        <h4>Add New Watchlist</h4>
+    
+                        <form method="POST" action="/watchlists">
+                            @csrf
+    
+                            <!-- <label for="title">Title</label> -->
+                            <input placeholder="Watchlist Title..."name="title" />
+                            <button type="submit">Submit</button>
+                        </form>
+                    </div>
+                </div>
+
+        <div class="watch-lists flex-col sp-be">
+            <div>
+            <h3>Your Reviews</h3>
             <ul>
                 @foreach($watchlists as $watchlist)
-                <div>
+                <div class="formform">
                     <form method="POST" action="/watchlists/{{ $watchlist['id'] }}/delete">
                         @csrf
 
@@ -37,7 +89,7 @@
                 @endforeach
             </ul>
             </div>
-                <div class="add-new"> 
+                {{-- <div class="add-new"> 
                     <h4>Add New Watchlist</h4>
 
                     <form method="POST" action="/watchlists">
@@ -47,8 +99,13 @@
                         <input placeholder="Watchlist Title..."name="title" />
                         <button type="submit">Submit</button>
                     </form>
-                </div>
+                </div> --}}
             </div>
+
+        </div>
+
+
+
         </div>
     </div>
 
