ed7141d7d86d403da55e9c3a6be4c93d72003a52
Esra Oktav Dinler <oktavesra@gmail.com>
2019-04-03 14:58:21 +0200

    :zap: Structure fixes styling changes etc + Just allowing oneline for description of characher/role

--- resources/views/pages/actorDetail.blade.php
+++ resources/views/pages/actorDetail.blade.php
@@ -4,22 +4,21 @@
 
 
 <div class="page-content">
-
 @if(isset($data) && count($data) > 0)
 
     @if(isset($data['tagged_images']['results']) && count($data['tagged_images']['results']) > 0)
         @foreach($data['tagged_images']['results'] as $backdrop)
-        @if ($loop->first)
-        <div class="mov-detail flex-col" 
-            style="background: url('https://image.tmdb.org/t/p/w1400_and_h450_face/{{$backdrop['media']['backdrop_path']}}');   
-            width: 100vw;
-            background-size: 100vw;
-            background-repeat: no-repeat;
-            display: flex;
-        ">
-        @endif
+            @if ($loop->first)
+            <div class="mov-detail flex-col" 
+                style="background: url('https://image.tmdb.org/t/p/w1400_and_h450_face/{{$backdrop['media']['backdrop_path']}}');   
+                width: 100vw;
+                background-size: 100vw;
+                background-repeat: no-repeat;
+                display: flex;
+            ">
+            @endif
         @endforeach
-        @else
+    @else
         <div class="mov-detail flex-col"
             style="background: url({{ asset('images/backdropPlaceholder.png') }});
             width: 100vw;
@@ -40,145 +39,85 @@
                         <img src='{{ asset('images/posterPlaceholder.png') }}' />
                     @endif
                
-    
-                    <h4 style="margin: 10px auto 4px auto; with:260px;display: flex; flex-flow: row; flex-wrap: wrap; width: 265px; text-align: center;">
-                    @if(isset($data['place_of_birth']))
-                        {{ $data['place_of_birth'] }}
+                <br/>
+                    <h4 style="margin: 10px auto 4px auto; justify-content: center; with:260px;display: flex; flex-flow: row; flex-wrap: wrap; width: 265px; text-align: center;">
+                        @if(isset($data['place_of_birth']))
+                            {{ $data['place_of_birth'] }}
+                        @endif   
+                    </h4>
+                <br/>
+                    <h5>
+                        @if(strlen($data['birthday']) > 1)
+                            {{$data['birthday']}}
                         @endif
-                        <br/>
-                        </h4>
-                <h5>
-                    @if(isset($data['place_of_birth']))
-                    {{ $data['place_of_birth'] }}
-                    @endif
-                       
-                </h5>
+                    </h5>
                 <br/>
-                <h5>
-                    @if(strlen($data['birthday']) > 1)
-                        {{$data['birthday']}}
-                    @endif
-                </h5>
-                <br/>
-                <h5>
-                   
-                    @if(strlen($data['deathday']) > 1)
-                        Died:{{$data['deathday']}}
-                    @endif
-                       
-                </h5>
-
-                
-          
-                
-                      
-                   
+                    <h5>
+                        @if(strlen($data['deathday']) > 1)
+                            Died: {{$data['deathday']}}
+                        @endif
+                    </h5>                
                 </div>
             </div>
         
             <div class="mov-flex-2 flex-col">
-        
                 <h1>{{$data['name']}}</h1>
-
-                <h5>
-                    @if(strlen($data['birthday']) > 1)
-                        {{$data['birthday']}}
-                    @endif
-                    <br/>
-                    @if(strlen($data['deathday']) > 1)
-                        Died:{{$data['deathday']}}
-                    @endif
-                </h5>
-
-                
-                @if(isset($data['place_of_birth']))
-                    {{ $data['place_of_birth'] }}
-                    @endif
-                
-             
-                
-                <div class="mov-flex-cast flex-row" style="height: 220px !important;">
-                @if(isset($data['movie_credits']['cast']) && count($data['movie_credits']['cast']) > 0)
-                    @foreach($data['movie_credits']['cast'] as $credits)
-                    <div class="actor">
-                        @if(isset($credits['release_date']))
-                        <p>{{ substr($credits['release_date'], 0 ,-6)}}</p> 
-                        @endif
-                        <a href='{{ url('details/' . $credits['id']) }}'>
-                            @if(isset($credits['poster_path']))
-                                <img src='https://image.tmdb.org/t/p/w500/{{$credits['poster_path']}}'/>
-                            @else
-                                <img src='{{ asset('images/castPlaceholder.png') }}' />
+                <div class="mov-flex-cast flex-row" >
+                    @if(isset($data['movie_credits']['cast']) && count($data['movie_credits']['cast']) > 0)
+                        @foreach($data['movie_credits']['cast'] as $credits)
+                        <div class="actor">
+                            <a href='{{ url('details/' . $credits['id']) }}'>
+                                @if(isset($credits['poster_path']))
+                                    <img src='https://image.tmdb.org/t/p/w500/{{$credits['poster_path']}}'/>
+                                @else
+                                    <img src='{{ asset('images/castPlaceholder.png') }}' />
+                                @endif
+                            </a>
+                            <h5>{{ $credits['title'] }}</h5>
+                            <p style="height: 15px; overflow: hidden">{{$credits['character']}}</p>   
+                            @if(isset($credits['release_date']))
+                                <p>{{ substr($credits['release_date'], 0 ,-6)}}</p> 
                             @endif
-                        </a>
-                        <h5>{{ $credits['title'] }}</h5>
-                        <p>{{$credits['character']}}</p>   
-                    </div>
-                    @endforeach 
-                @endif
+                        </div>
+                        @endforeach 
+                    @endif
                 </div> 
 
-                {{--<div class="mov-flex-cast flex-row">
-                @if(isset($data['tagged_images']['results']) && count($data['tagged_images']['results']) > 0)
-                    @foreach($data['tagged_images']['results'] as $caste)
-                    <div class="actor">
-                        <a href='{{ url('actor/' . $caste['vote_count']) }}'>
-                            @if(isset($caste['file_path']))
-                                <img src='https://image.tmdb.org/t/p/w500/{{$caste['file_path']}}'/>
-                            @else
-                                <img src='{{ asset('images/castPlaceholder.png') }}' />
-                            @endif
-                        </a>
-
-                    </div>
-                    @endforeach 
-                @endif
-                </div>--}}
-
                 @if(isset($data['biography']))
+                    <h5 style="color: black; text-shadow: none; font-size: 15px;">Biography</h5>
                     <p>{{ $data['biography'] }}</p>
                 @endif
-
-                
             </div>
-        </div>
+           
+        </div>              
+    </div>     
     
-                    
-    </div>       
+    
     @if(isset($data['tagged_images']['results']) && count($data['tagged_images']['results']) > 0)
     <div class="similar-container flex-col">
-            <div class="similar-content">
-          
-            <h1>Photos</h1>
-           
-                <div class="similar-flex flex-row">
+        <div class="similar-content">   
+        <h1>Photos</h1>
+            <div class="similar-flex flex-row">
                 @if(isset($data['tagged_images']['results']) && count($data['tagged_images']['results']) > 0)
                     @foreach($data['tagged_images']['results'] as $caste)
                     <div class="similar-mov">
                         <a href='{{ url('details/' . $caste['media']['id']) }}'>
-                        @if(isset($caste['file_path']))
+                            @if(isset($caste['file_path']))
                                 <img style="width: auto !important;" src='https://image.tmdb.org/t/p/w500/{{$caste['file_path']}}'/>
                             @else
                                 <img src='{{ asset('images/castPlaceholder.png') }}' />
                             @endif
                         </a>
-                        @if(isset($caste['media']['release_date']))
+                        @if(isset($caste['media']['title']))
                         {{ $caste['media']['title'] }}
                         @endif
-
-                         {{-- @if(isset($caste['media']['release_date']))
-                        {{ substr($caste['media']['release_date'], 0 ,-6)}}
-                        @endif--}} 
                     </div>
-                 
-                    
                     @endforeach 
                 @endif
-                </div>
-            
             </div>
-            @endif
-        </div>
+        </div>       
+    </div>
+    @endif
 
 
     @elseif(isset($error_msg))
@@ -187,9 +126,7 @@
         </h3>
     @endif
         
-    </div>
-
-    
+</div>    
 @endsection
 
 
